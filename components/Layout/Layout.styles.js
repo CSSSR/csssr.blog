bc7292ba17524420570dcfd318a38f69ed830a64
Azat Ayupov <runor1@yandex.ru>
2020-04-30 14:33:37 +0500

    add fonts

---
+++ components/Layout/Layout.styles.js
@@ -0,0 +1,14 @@
+import { css } from '@emotion/core'
+
+export const fonts = css `
+  html, body {
+    font-family: 'Roboto', sans-serif;
+  }
+
+  .styledTitle {
+    font-family: 'Amita', cursive;
+  }
+`
+export default css`
+  ${fonts}
+`
