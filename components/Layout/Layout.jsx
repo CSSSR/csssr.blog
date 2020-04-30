3a65495b9070613050adc5754a855d0627380a53
Ayupov Azat <Runor1@yandex.ru>
2020-04-30 16:08:29 +0500

    COM-1799: Подключение кастомных шрифтов #1

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,11 +1,15 @@
-import Alert from '../components/alert'
-import Footer from '../components/footer'
-import Meta from '../components/meta'
+import { Global } from '@emotion/core'
+
+import Alert from '../alert'
+import Footer from '../footer'
+import Meta from '../meta'
+import styles from './Layout.styles'
 
 export default function Layout({ preview, children }) {
   return (
     <>
       <Meta />
+      <Global styles={styles} />
       <div className="min-h-screen">
         <Alert preview={preview} />
         <main>{children}</main>
--- components/meta.js
+++ components/meta.js
@@ -37,6 +37,7 @@ export default function Meta() {
         content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
       />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+      <link href="https://fonts.googleapis.com/css2?family=Amita:wght@400;700&family=Roboto&display=swap" rel="stylesheet" />
     </Head>
   )
 }
