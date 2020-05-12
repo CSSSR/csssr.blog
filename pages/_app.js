4bf5be0e59d44a435fb627c3fec02968d7a0f4c8
Artyom Resh <artyomresh@gmail.com>
2020-05-12 17:23:41 +0300

    feat: COM-1802 добавляет конфиг для сборки стендов

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import { ThemeProvider } from 'emotion-theming'
 import { Root, defaultTheme } from '@csssr/core-design'
 
-console.log('xxx', defaultTheme)
 const MyApp = ({ Component, pageProps }) => (
   <Root>
     <ThemeProvider theme={defaultTheme}>
