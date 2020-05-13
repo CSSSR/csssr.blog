f55d43235c0a535b2fa3a5660c50bcf6a45e9f37
Artyom Resh <artyomresh@gmail.com>
2020-05-13 11:04:54 +0300

    feat: добавляет логику управления outlin'ом

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,27 @@
+import { useEffect } from 'react'
 import { ThemeProvider } from 'emotion-theming'
 import { Root, defaultTheme } from '@csssr/core-design'
 
-const MyApp = ({ Component, pageProps }) => (
-  <Root>
-    <ThemeProvider theme={defaultTheme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  </Root>
-)
+const MyApp = ({ Component, pageProps }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', function (event) {
+      if (event.which === 9) {
+        document.body.classList.add('outline')
+      }
+    })
+
+    window.addEventListener('click', function () {
+      document.body.classList.remove('outline')
+    })
+  })
+
+  return (
+    <Root>
+      <ThemeProvider theme={defaultTheme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Root>
+  )
+}
 
 export default MyApp
