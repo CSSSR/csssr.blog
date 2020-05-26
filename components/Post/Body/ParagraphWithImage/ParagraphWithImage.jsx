addbc442d09f53496ddd8c6fad5134d8fc7bd88f
Viktor Kosinov <vkosinov@ya.ru>
2020-05-26 14:13:21 +0300

    fix: COM-1902 рефакторинг ParagraphWithImage

--- components/Post/Body/ParagraphWithImage/ParagraphWithImage.jsx
+++ components/Post/Body/ParagraphWithImage/ParagraphWithImage.jsx
@@ -1,29 +1,13 @@
 import React from 'react'
 import { string, node } from 'prop-types'
 import styled from '@emotion/styled'
-import { useTheme } from 'emotion-theming'
+import Img from '../Img'
 import styles from './ParagraphWithImage.styles'
 
 const ParagraphWithImage = ({ children, className, slug, imgName }) => {
-  const theme = useTheme()
-
   return (
     <div className={className}>
-      <picture className="img-wrap">
-        <source
-          media={theme.breakpoints.desktop.all.slice(7)}
-          type="image/png"
-          srcSet={`/assets/blog/posts/${slug}/desktop.all/${imgName}.png`}
-        />
-
-        <source
-          media={theme.breakpoints.tablet.all.slice(7)}
-          type="image/png"
-          srcSet={`/assets/blog/posts/${slug}/tablet.all/${imgName}.png`}
-        />
-
-        <img src={`/assets/blog/posts/${slug}/mobile.all/${imgName}.png`} alt={imgName} />
-      </picture>
+      <Img name={imgName} slug={slug} className="img-wrap" />
       {children}
     </div>
   )
