76b873bb838b4603a07db61dfbb0f992317243f1
Viktor Kosinov <vkosinov@ya.ru>
2020-05-28 18:06:33 +0300

    fix: COM-1965 Исправил размытые картинки

--- components/Post/Body/Img/Img.styles.js
+++ components/Post/Body/Img/Img.styles.js
@@ -7,6 +7,9 @@ const base = ({ breakpoints: { desktop, tablet, mobile }}) => css`
   }
 
   img {
+    max-width: 656px;
+    margin-left: auto;
+    margin-right: auto;
     display: block;
     height: 100%;
     width: 100%;
