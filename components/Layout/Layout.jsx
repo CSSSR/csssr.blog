1b28e7dc0a29a8bc032656b53a880ddd4175c77b
Viktor Kosinov <vkosinov@ya.ru>
2020-05-28 16:44:01 +0300

    fix: COM-1943 Исправил название ссылки

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -2,6 +2,7 @@ import { Global } from '@emotion/core'
 import Meta from '../Meta'
 import styles from './Layout.styles'
 import { Header, Footer } from '@csssr/core-design'
+import { nav } from '../../data/footerLinks'
 
 export default function Layout({ children }) {
   return (
@@ -10,7 +11,7 @@ export default function Layout({ children }) {
 
       <Header actionButton={{ isVisible: false }} />
       <main id="main">{children}</main>
-      <Footer className="footer" />
+      <Footer className="footer" nav={nav} />
 
       <Global styles={styles} />
     </>
---
+++ data/footerLinks.js
@@ -0,0 +1,107 @@
+export const nav = [
+  {
+    id: 'services',
+    title: 'Services',
+    links: [
+      {
+        id: 'outsourcing',
+        title: 'Outsourcing',
+        href: 'https://csssr.com/en/service/outsourcing-front-end',
+      },
+      {
+        id: 'express',
+        title: 'Express',
+        href: 'https://csssr.com/en/service/express-front-end',
+      },
+      {
+        id: 'mvp',
+        title: 'MVP',
+        href: 'https://csssr.com/en/service/mvp-development',
+      },
+      {
+        id: 'backend',
+        title: 'Back-end</br>and DevOps',
+        href: 'https://csssr.com/en/service/back-end-and-devops',
+      },
+    ],
+  },
+  {
+    id: 'industries',
+    title: 'Industries',
+    links: [
+      {
+        id: 'fintech',
+        title: 'Fintech',
+        href: 'https://csssr.com/en/industry/fintech',
+      },
+      {
+        id: 'information',
+        title: 'IT companies',
+        href: 'https://csssr.com/en/industry/information-technology',
+      },
+      {
+        id: 'eCommerce',
+        title: 'E-Commerce',
+        href: 'https://csssr.com/en/industry/ecommerce',
+      },
+      {
+        id: 'mediaAndMarketing',
+        title: 'Media and marketing',
+        href: 'https://csssr.com/en/industry/media-and-marketing',
+      },
+    ],
+  },
+  {
+    id: 'howWeWork',
+    title: 'How we work',
+    links: [
+      {
+        id: 'technologies',
+        title: 'Technologies',
+        href: 'https://csssr.com/en/tech-stack',
+      },
+      {
+        id: 'wayOfWork',
+        title: 'Process',
+        href: 'https://csssr.com/en/way-of-work',
+      },
+      {
+        id: 'coreValues',
+        title: 'Core values',
+        href: 'https://csssr.com/en/core-values',
+      },
+    ],
+  },
+  {
+    id: 'products',
+    title: 'Products',
+    links: [
+      {
+        id: 'lms',
+        title: 'Modular LMS system',
+        href: 'https://csssr.com/en/products/lms',
+      },
+      {
+        id: 'tracker',
+        title: 'Time-tracking softwarecess',
+        href: 'https://tracker.csssr.com/',
+      },
+    ],
+  },
+  {
+    id: 'company',
+    title: 'Company',
+    links: [
+      {
+        id: 'careers',
+        title: 'Careers',
+        href: 'https://csssr.com/en/jobs',
+      },
+      {
+        id: 'blog',
+        title: 'Blog',
+        href: 'https://blog.csssr.com',
+      },
+    ],
+  },
+]
