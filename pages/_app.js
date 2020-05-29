11fe0c54e7ee3a41b85ccd551b5f8e3d11b5339e
yankovskyandrey <YankovskyAndrey@gmail.com>
2020-05-29 14:57:24 +0300

    COM-1996: подключил sentry

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,12 @@ import { useEffect } from 'react'
 import { ThemeProvider } from 'emotion-theming'
 import { Root, defaultTheme } from '@csssr/core-design'
 import Prism from 'prismjs'
+import '../utils/sentry'
 
 const MyApp = ({ Component, pageProps }) => {
   useEffect(() => {
     window.addEventListener('keydown', function (event) {
+      a()
       if (event.which === 9) {
         document.body.classList.add('outline')
       }
