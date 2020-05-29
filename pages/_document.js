340ac31ffd8cefebdd5c722460678776c8886bef
yankovskyandrey <YankovskyAndrey@gmail.com>
2020-05-29 14:40:55 +0300

    COM-1948: добавил gtm

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,18 @@
+import { GtmNoScript, GtmScript } from 'react-gtm-components'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import getGtmId from '../utils/getGtmId'
 
 export default class MyDocument extends Document {
   render() {
+    const gtmId = getGtmId()
+
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <GtmScript gtmId={gtmId} />
+        </Head>
         <body>
+          <GtmNoScript gtmId={gtmId} />
           <Main />
           <NextScript />
         </body>
