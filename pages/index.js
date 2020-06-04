3e08089e13528affa9ce01be7c49592fcd5ef0e5
yankovskyandrey <YankovskyAndrey@gmail.com>
2020-06-04 16:44:53 +0300

    COM-2016: поправил ошибку eslint

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ import Head from 'next/head'
 const Index = () => (
   <Head>
     <meta httpEquiv="refresh" content="0; URL='https://blog.csssr.com/en'" />
-    <script dangerouslySetInnerHTML={{__html: 'window.location.replace("https://blog.csssr.com/en")'}} />
+    <script
+      dangerouslySetInnerHTML={{ __html: 'window.location.replace("https://blog.csssr.com/en")' }}
+    />
   </Head>
 )
 export default Index
