2d9fed228c4a0e95cda88386cd8ecbf820b4f7ea
yankovskyandrey <YankovskyAndrey@gmail.com>
2020-06-05 12:05:41 +0300

    Сделал так, что редирект с / работает для всех стендов

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,8 @@ import Head from 'next/head'
 
 const Index = () => (
   <Head>
-    <meta httpEquiv="refresh" content="0; URL='https://blog.csssr.com/en'" />
-    <script
-      dangerouslySetInnerHTML={{ __html: 'window.location.replace("https://blog.csssr.com/en")' }}
-    />
+    <meta httpEquiv="refresh" content="0; URL='/en'" />
+    <script dangerouslySetInnerHTML={{ __html: 'window.location.replace("/en")' }} />
   </Head>
 )
 export default Index
