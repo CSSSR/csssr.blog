0249fab849a51cacc112dbe79711df521f7559a6
yankovskyandrey <YankovskyAndrey@gmail.com>
2020-06-18 16:36:33 +0300

    Добавил отправку данных lh на наш сервер

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,7 +4,9 @@ module.exports = {
       numberOfRuns: 5,
     },
     upload: {
-      target: 'temporary-public-storage',
+      target: 'lhci',
+      serverBaseUrl: 'https://lhci.csssr.cloud',
+      token: 'd9aff931-87e0-402e-985c-123d5a3bdab2',
     },
     // assert: {
     //   preset: 'lighthouse:recommended',
