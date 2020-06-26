f35244b3a622af875934ccd9e9602e2c0918143f
Alexandr Kulakov <BenuelBertran@gmail.com>
2020-06-26 15:29:15 +0300

    Replace text wrapper p to div

--- components/Post/Body/Quote/Quote.jsx
+++ components/Post/Body/Quote/Quote.jsx
@@ -4,16 +4,23 @@ import styled from '@emotion/styled'
 import styles from './Quote.styles'
 import { Grid } from '@csssr/core-design'
 import { PictureSmart } from '@csssr/csssr.images/dist/react'
+import { Text } from '@csssr/core-design'
 
 const Quote = ({ className, children, author, position, withImage }) => {
   return (
     <Grid className={className}>
       {withImage && (
-        <PictureSmart className="image-wrapper" alt="lady" requireImages={require.context('../../../../public/components/quote/images')} />
+        <PictureSmart
+          className="image-wrapper"
+          alt="lady"
+          requireImages={require.context('../../../../public/components/quote/images')}
+        />
       )}
 
       <blockquote className="quote">
-        <p className="quote-text font_p16-regular">{children}</p>
+        <Text className="quote-text font_p16-regular" as="div">
+          {children}
+        </Text>
         <span className="author font_h3-regular ">{author}</span>
         <span className="autor-post font_perforator-10-regular">{position}</span>
       </blockquote>
