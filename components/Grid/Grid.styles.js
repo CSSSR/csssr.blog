0978ec3e3f6b192680d9ff5711efe0a91fcfe7a3
Viktor Kosinov <vkosinov@ya.ru>
2020-06-26 17:01:25 +0300

    feat: COM-2087 Поправил сетку

--- components/Grid/Grid.styles.js
+++ components/Grid/Grid.styles.js
@@ -1,11 +1,6 @@
 import { css } from '@emotion/core'
 
 const base = ({ breakpoints: { desktop }}) => css`
-  ${desktop.m} {
-    grid-template-columns: repeat(12,96px);
-    grid-gap: 0 16px;
-    width: 1328px;
-  }
 
   ${desktop.l} {
     grid-template-columns: repeat(12,96px);
