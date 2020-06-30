356d214b6638202d99fb537885375218cbbf7367
Alexandr Kulakov <BenuelBertran@gmail.com>
2020-06-30 11:43:45 +0300

    Replace Text component to div tag

--- components/Post/Body/Quote/Quote.jsx
+++ components/Post/Body/Quote/Quote.jsx
@@ -4,7 +4,6 @@ import styled from '@emotion/styled'
 import styles from './Quote.styles'
 import { Grid } from '@csssr/core-design'
 import { PictureSmart } from '@csssr/csssr.images/dist/react'
-import { Text } from '@csssr/core-design'
 
 const Quote = ({ className, children, author, position, withImage }) => {
   return (
@@ -18,9 +17,7 @@ const Quote = ({ className, children, author, position, withImage }) => {
       )}
 
       <blockquote className="quote">
-        <Text className="quote-text font_p16-regular" as="div">
-          {children}
-        </Text>
+        <div className="quote-text font_p16-regular">{children}</div>
         <span className="author font_h3-regular ">{author}</span>
         <span className="autor-post font_perforator-10-regular">{position}</span>
       </blockquote>
