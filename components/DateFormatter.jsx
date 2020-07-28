bdd731075b478ca74280b202428ff03649f27a43
Viktor Kosinov <vkosinov@ya.ru>
2020-07-28 11:42:54 +0300

    COM-2064: Add more obvious condition

--- components/DateFormatter.jsx
+++ components/DateFormatter.jsx
@@ -4,7 +4,7 @@ import { ru } from 'date-fns/locale'
 // TODO: может потом пригодится
 export default function DateFormatter({ children, className, language }) {
   const date = parseISO(children)
-  const locale = language === 'ru' && ru
+  const locale = language === 'ru' ? ru : false
   const localeFormat = language === 'ru' ? 'd MMMM, yyyy' : 'LLLL d, yyyy'
   return (
     <time className={className} dateTime={children}>
