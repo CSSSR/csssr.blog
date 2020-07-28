916b6c304421cda3cbab86fe9a9581927a2f4b54
Viktor Kosinov <vkosinov@ya.ru>
2020-07-28 09:56:55 +0300

    COM-2064: Fix extra spaces

--- components/DateFormatter.jsx
+++ components/DateFormatter.jsx
@@ -5,7 +5,7 @@ import { ru } from 'date-fns/locale'
 export default function DateFormatter({ children, className, language }) {
   const date = parseISO(children)
   const locale = language === 'ru' && ru
-  const localeFormat = language === 'ru' ? 'd	MMMM, yyyy' : 'LLLL	d, yyyy'
+  const localeFormat = language === 'ru' ? 'd MMMM, yyyy' : 'LLLL d, yyyy'
   return (
     <time className={className} dateTime={children}>
       {format(date, localeFormat, { locale })}
