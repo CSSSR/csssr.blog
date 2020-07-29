747f1912e2fe2c2e6014e8e0802428fdff6d4605
slava <slavaavals616@gmail.com>
2020-07-29 17:21:30 +0400

    update to 1.0.59

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,13 +1,7 @@
 import Link from 'next/link'
 import { Global } from '@emotion/core'
 import { Footer } from '@csssr/core-design'
-import {
-  Header,
-  getLocaleFromUrl,
-  PageContent,
-  theme,
-  getNavbarOpenStateFromQuery,
-} from '@csssr/csssr-shared-header'
+import { Header, getLocaleFromUrl, PageContent, theme } from '@csssr/csssr-shared-header'
 import Meta from '../Meta'
 import { withRouter } from 'next/router'
 import styles from './Layout.styles'
@@ -17,20 +11,13 @@ import { ThemeProvider } from 'emotion-theming'
 function Layout({ children, router }) {
   const appRootElement = typeof window === 'object' ? document.getElementById('__next') : null
   const lng = getLocaleFromUrl(router.asPath)
-  const isNavbarOpen = getNavbarOpenStateFromQuery(router.asPath.split('?')[1])
 
   return (
     <>
       <Meta />
 
       <ThemeProvider theme={theme}>
-        <Header
-          pathname="blog"
-          lng={lng}
-          NextLink={Link}
-          appRootElement={appRootElement}
-          isOpen={isNavbarOpen}
-        />
+        <Header pathname="blog" lng={lng} NextLink={Link} appRootElement={appRootElement} />
       </ThemeProvider>
 
       <PageContent>
