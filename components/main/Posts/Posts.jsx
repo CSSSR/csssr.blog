fd1692cb0f9b2ee822b55f5d45448e35bd20170f
Artyom Resh <artyomresh@gmail.com>
2020-08-14 12:34:08 +0300

    fix(Posts): COM-2425
    - remove postOrder check in Posts render method

--- components/main/Posts/Posts.jsx
+++ components/main/Posts/Posts.jsx
@@ -4,44 +4,21 @@ import { Grid } from '../../Grid'
 import styled from '@emotion/styled'
 import styles from './Posts.styles'
 import PostCard from './PostCard'
-import postsOrderEn from '../../../postsOrderEn'
-import postsOrderRu from '../../../postsOrderRu'
 
-const postsOrder = {
-  en: postsOrderEn,
-  ru: postsOrderRu,
-}
+const Posts = ({ language, posts, className }) => (
+  <Grid as="ul" className={className}>
+    {posts.reduce((memo, post) => {
+      const orderInGroupOfFour = memo.length % 4
+      const size = orderInGroupOfFour === 0 || orderInGroupOfFour === 3 ? 'l' : 's'
+      const side = orderInGroupOfFour === 0 || orderInGroupOfFour === 2 ? 'l' : 'r'
 
-// В props.posts приходят статьи, которые надо отобразить на странице.
-// В postsOrder перечислены все статьи.
-// Проходим по postsOrder и набираем по этому порядку посты в соответствии с props.posts.
-const Posts = ({ language, posts, className }) => {
-  const postsBySlug = posts.reduce((acc, post) => {
-    acc[post.slug] = post
-
-    return acc
-  }, {})
-
-  return (
-    <Grid as="ul" className={className}>
-      {postsOrder[language].flat().reduce((memo, postSlug) => {
-        const post = postsBySlug[postSlug]
-        if (!post) {
-          return memo
-        }
-
-        const orderInGroupOfFour = memo.length % 4
-        const size = orderInGroupOfFour === 0 || orderInGroupOfFour === 3 ? 'l' : 's'
-        const side = orderInGroupOfFour === 0 || orderInGroupOfFour === 2 ? 'l' : 'r'
-
-        return [
-          ...memo,
-          <PostCard key={postSlug} language={language} post={post} size={size} side={side} />,
-        ]
-      }, [])}
-    </Grid>
-  )
-}
+      return [
+        ...memo,
+        <PostCard key={post.slug} language={language} post={post} size={size} side={side} />,
+      ]
+    }, [])}
+  </Grid>
+)
 
 Posts.propTypes = {
   className: string,
