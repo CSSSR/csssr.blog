631fd87582f9c045b8917ee249da09dacb597967
Artyom Resh <artyomresh@gmail.com>
2020-12-11 19:29:57 +0300

    fix: COM-2976 Update getBenchmarkEmailListId

--- utils/getBenchmarkEmailListId.js
+++ utils/getBenchmarkEmailListId.js
@@ -1,6 +1,6 @@
 export default () => {
-  if (process.env.IS_PRODUCTION) {
-    return '18030400'
+  if (!process.env.IS_PRODUCTION) {
+    return '17171975'
   }
-  return '17171975'
+  return '18030400'
 }
