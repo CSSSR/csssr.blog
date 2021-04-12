790c295306c7fece25a3fc6508cc26ed53f47c28
Viktor Kosinov <vkosinov@ya.ru>
2021-04-12 17:21:15 +0300

    feat(components): COM-3432 Add new Error404Page

--- pages/[language]/404.js
+++ pages/[language]/404.js
@@ -1,7 +1,8 @@
-import Error404Page from '../../components/error/Error404Page'
 import React from 'react'
 
-// This page is placed here for testing purposes
+import Error404Page from '../components/error/Error404Page'
+import { getPostsByLanguage } from '../lib/api'
+
 class Custom404 extends React.Component {
   render() {
     return <Error404Page {...this.props} />
@@ -9,3 +10,21 @@ class Custom404 extends React.Component {
 }
 
 export default Custom404
+
+export async function getStaticProps() {
+  const posts = await getPostsByLanguage([
+    'title',
+    'date',
+    'slug',
+    'author',
+    'coverImageAlt',
+    'tag',
+    'images',
+  ])
+
+  return {
+    props: {
+      posts,
+    },
+  }
+}
