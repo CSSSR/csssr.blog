c7621b5890041d5739016cf8de87b8b124e63d39
Sergei <epcijer@gmail.com>
2021-04-14 12:44:50 +0300

    feat: COM-3456 linting

--- components/Post/Header/NewsAudioLink/NewsAudioLink.jsx
+++ components/Post/Header/NewsAudioLink/NewsAudioLink.jsx
@@ -8,28 +8,22 @@ import { ReactComponent as Arrow } from '../../../../public/components/newsAudio
 import { ReactComponent as RightArrow } from '../../../../public/components/newsAudioLink/icons/right-arrow.svg'
 
 const NewsAudioLink = ({ className, href, testId = 'NewsAudio:link' }) => (
-  <div
-    className={className}
-    data-testid={testId}
-  >
+  <div className={className} data-testid={testId}>
     <div className="soundtrack-wrapper">
       <div className="soundtrack-image" />
     </div>
 
     <div className="links-wrapper">
-      <a className="link"
-         href="https://radio.csssr.com/rss/news512.rss"
-         target="_blank"
-         rel="noopener nofollow"
+      <a
+        className="link"
+        href="https://radio.csssr.com/rss/news512.rss"
+        target="_blank"
+        rel="noopener nofollow"
       >
         {'RSS подкаста'}
         <RightArrow className="arrow" />
       </a>
-      <a className="link"
-         href={href}
-         target="_blank"
-         rel="noopener nofollow"
-      >
+      <a className="link" href={href} target="_blank" rel="noopener nofollow">
         Прослушать аудио-версию
         <Arrow className="arrow" />
       </a>
