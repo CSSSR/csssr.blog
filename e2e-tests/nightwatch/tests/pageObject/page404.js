6181b1f6fdc1f9a57a0284030741fcacd3cb6f54
Nikita Emelyanov <nik.testbom@gmail.com>
2021-05-24 10:03:38 +0300

    COM-3594: added tests for the 404 page

---
+++ e2e-tests/nightwatch/tests/pageObject/page404.js
@@ -0,0 +1,15 @@
+module.exports = {
+  block: {
+    main: '[data-testid="Header:link.logo"]',
+    page404: '[data-testid="ErrorPage404:block"]',
+    postsList:'[data-testid="ErrorPage404:block:posts.list"]',
+  },
+  link: {
+    all: '[data-testid="ErrorCategories:link:all"]',
+    management: '[data-testid="ErrorCategories:link:management"]',
+    development: '[data-testid="ErrorCategories:link:web-development"]',
+    culture: '[data-testid="ErrorCategories:link:culture"]',
+    news: '[data-testid="ErrorCategories:link:news"]',
+    business: '[data-testid="ErrorCategories:link:business"]',
+  }
+}
