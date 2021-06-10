b5417871e05450026f53d01bd550ae58504ffef2
Viktor Kosinov <vkosinov@ya.ru>
2021-06-10 09:34:04 +0300

    feat:COM-3646 Add rule in getDescription

--- utils/client/getDescription.js
+++ utils/client/getDescription.js
@@ -11,16 +11,18 @@ export default (post, isNews) => {
 
   const content = post?.content || post
 
-  return (
-    content
-      .slice(content.indexOf('**'), content.indexOf('\n', content.indexOf('**')))
-      .replace('\n', '')
-      .replace(/\**/g, '')
-      .replace(/&nbsp;/g, ' ')
-      // eslint-disable-next-line no-irregular-whitespace
-      .replace(/ /g, ' ')
-      .replace(/<.*?>/g, '')
-      .replace(/\[|]\(https?:\/\/?[\da-z.-]+\.[a-z.]{2,6}(?:[/\w .-]*)*\/?\)/g, '')
-      .replace(/\s{2,}/g, ' ')
-  )
+  if (content) {
+    return (
+      content
+        .slice(content.indexOf('**'), content.indexOf('\n', content.indexOf('**')))
+        .replace('\n', '')
+        .replace(/\**/g, '')
+        .replace(/&nbsp;/g, ' ')
+        // eslint-disable-next-line no-irregular-whitespace
+        .replace(/ /g, ' ')
+        .replace(/<.*?>/g, '')
+        .replace(/\[|]\(https?:\/\/?[\da-z.-]+\.[a-z.]{2,6}(?:[/\w .-]*)*\/?\)/g, '')
+        .replace(/\s{2,}/g, ' ')
+    )
+  }
 }
