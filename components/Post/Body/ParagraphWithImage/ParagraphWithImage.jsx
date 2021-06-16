af8a542268d387ca2fc71f14292672c3ca6906a1
Viktor Kosinov <vkosinov@ya.ru>
2021-06-16 17:14:24 +0300

    feat:COM-3653 Rename defaultImages

--- components/Post/Body/ParagraphWithImage/ParagraphWithImage.jsx
+++ components/Post/Body/ParagraphWithImage/ParagraphWithImage.jsx
@@ -3,15 +3,13 @@ import styled from '@emotion/styled'
 import { node, string } from 'prop-types'
 import React from 'react'
 
-import defaultImages from './constants/images'
+import images from './constants/images'
 import styles from './ParagraphWithImage.styles'
 
 const ParagraphWithImage = ({ children, className, alt, imageName }) => {
   return (
     <div className={className}>
-      {defaultImages[imageName] && (
-        <Picture sources={defaultImages[imageName]} className="img-wrap" alt={alt} />
-      )}
+      {images[imageName] && <Picture sources={images[imageName]} className="img-wrap" alt={alt} />}
       {children}
     </div>
   )
